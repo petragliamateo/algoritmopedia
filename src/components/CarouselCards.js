e482133a3c396f68399e3242d56c1948d8b33f57
Mateo <petragliamateo@gmail.com>
2022-08-13 18:23:34 -0300

    categories 2

---
+++ src/components/CarouselCards.js
@@ -0,0 +1,77 @@
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react-native/no-inline-styles */
+import {
+  View, useWindowDimensions, Pressable,
+} from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+
+import { SubTitle, RegularText } from '../customComponents/TextComponents';
+
+function CarouselCards({ categorias }) {
+  const isCarousel = React.useRef(null);
+  const [cardIndex, setCardIndex] = React.useState(0);
+  const navigation = useNavigation();
+  const { width } = useWindowDimensions();
+  const cardWidth = width - 60;
+  // Largo de la targeta = windowWidth - marginX * 2 - paddingCont * 2.
+
+  function renderCard({ item, index }) {
+    const cardStyle = {
+      width: cardWidth,
+      padding: 20,
+      borderWidth: 1,
+      borderRadius: 5,
+      alignSelf: 'center',
+    };
+    const commonTextCard = {
+      color: '#ffffff',
+      textAlign: 'center',
+      marginVertical: 15,
+    };
+    return (
+      <Pressable
+        key={item.name}
+        style={cardStyle}
+        onPress={() => navigation.navigate('categorias', { category: item })}
+      >
+        <SubTitle style={commonTextCard}>{item.name}</SubTitle>
+        <RegularText style={commonTextCard}>{item.description}</RegularText>
+      </Pressable>
+    );
+  }
+
+  return (
+    <View>
+      <Carousel
+        layout="default"
+        ref={isCarousel}
+        data={categorias}
+        renderItem={renderCard}
+        sliderWidth={width}
+        itemWidth={width}
+        onSnapToItem={(i) => setCardIndex(i)}
+        slideStyle={{ justifyContent: 'center' }}
+      />
+      <Pagination
+        dotsLength={categorias.length}
+        activeDotIndex={cardIndex}
+        carouselRef={isCarousel}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 0,
+          backgroundColor: '#ccc',
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+        tappableDots
+      />
+    </View>
+  );
+}
+
+export default CarouselCards;
--- src/containers/Categorias.js
+++ src/containers/Categorias.js
@@ -1,14 +1,13 @@
+/* eslint-disable react-native/no-raw-text */
 /* eslint-disable react-native/no-inline-styles */
 import {
-  Text, View, StyleSheet, ScrollView, useWindowDimensions, Pressable,
+  View, StyleSheet,
 } from 'react-native';
 import React, { useContext } from 'react';
-import { useNavigation } from '@react-navigation/native';
 
-import { Title, SubTitle, RegularText } from '../customComponents/TextComponents';
+import { Title } from '../customComponents/TextComponents';
 import AlgoritmosContext from '../contexts/AlgoritmosContext';
-
-// Estilo: que las tarjetas se pasen una por una, tipo publicacion de IG con circulitos abajo.
+import CarouselCards from '../components/CarouselCards';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,46 +16,14 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'space-around',
   },
-  horizontalScroller: {
-    maxHeight: '50%',
-  },
-  commonTextCard: {
-    color: '#ffffff',
-    textAlign: 'center',
-    marginVertical: 15,
-  },
 });
 
 function Categorias() {
   const { categorias } = useContext(AlgoritmosContext);
-
-  const navigation = useNavigation();
-  const { width } = useWindowDimensions();
-  // Largo de la targeta = windowWidth - marginX * 2 - paddingCont * 2.
-  const cardWidth = width - 60;
-  const cardStyle = {
-    width: cardWidth,
-    padding: 20,
-    borderWidth: 1,
-    borderRadius: 5,
-    marginHorizontal: 25,
-    alignSelf: 'flex-start',
-  };
   return (
     <View style={styles.container}>
-      <Title><Text style={{ color: '#ffffff' }}>📝 Categorías</Text></Title>
-      <ScrollView horizontal style={styles.horizontalScroller}>
-        {categorias.map((cat) => (
-          <Pressable
-            key={cat.name}
-            style={cardStyle}
-            onPress={() => navigation.navigate('categorias', { category: cat })}
-          >
-            <SubTitle style={styles.commonTextCard}>{cat.name}</SubTitle>
-            <RegularText style={styles.commonTextCard}>{cat.description}</RegularText>
-          </Pressable>
-        ))}
-      </ScrollView>
+      <Title style={{ color: '#ffffff' }}>📝 Categorías</Title>
+      <CarouselCards categorias={categorias} />
     </View>
   );
 }
